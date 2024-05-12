449561fa4986a0ca11d87fb037931ea4df4c2188
shaown224 <60014974+shaown224@users.noreply.github.com>
2024-05-13 00:03:35 +0600

    file structure change

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!os&h2-zafylrqes$g9=spk^z@$nt7$shx39+zp7!j)xp$%7j)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+print(SECRET_KEY);
 
 # SP api authetication keys
 LWA_AUTHENTICATION = {
@@ -107,7 +108,7 @@ DATABASES = {
     }
 }
 
-# Default setting 
+# Default setting
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
